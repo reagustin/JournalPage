ee4808aae75c65eef23a31b94b46ef5483f40161
Agustin <re_agustin@hotmail.com>
2022-08-17 15:45:15 -0300

    layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>      
-    </>
+    <JournalLayout>
+      <Typography>Excepteur est voluptate labore nostrud elit officia tempor est enim magna do. Eu nostrud enim nisi do. Excepteur est in aute voluptate qui deserunt proident ea aute ullamco pariatur. Lorem adipisicing cupidatat quis laborum nostrud ex enim in nisi aliquip laboris anim sit. Reprehenderit occaecat aliqua culpa laboris sint consequat non mollit elit laboris veniam. Ullamco et est ex non veniam eu.</Typography>
+    
+    {/* Nothing selected */}
+
+    {/* NoteView */}
+    
+    </JournalLayout>
   )
 }
