5e568708745d38e10489268d4868d394b1390c68
Agustin <re_agustin@hotmail.com>
2022-08-20 00:01:28 -0300

    imageGallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Excepteur est voluptate labore nostrud elit officia tempor est enim magna do. Eu nostrud enim nisi do. Excepteur est in aute voluptate qui deserunt proident ea aute ullamco pariatur. Lorem adipisicing cupidatat quis laborum nostrud ex enim in nisi aliquip laboris anim sit. Reprehenderit occaecat aliqua culpa laboris sint consequat non mollit elit laboris veniam. Ullamco et est ex non veniam eu.</Typography> */}
     
-    <NothingSelectedView/>
+    {/* <NothingSelectedView/> */}
 
-    {/* NoteView */}
+    <NoteView/>
     
     </JournalLayout>
   )
