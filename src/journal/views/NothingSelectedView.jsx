f94d18c34916ad5a1f9c8a76db72a462a66a568a
Agustin <re_agustin@hotmail.com>
2022-09-04 22:07:59 -0300

    finjournalSeccion19

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -5,7 +5,8 @@ import { Grid, Typography } from "@mui/material"
 
 export const NothingSelectedView = () => {
   return (
-    <Grid 
+    <Grid
+      className="animate__animated animate__fadeIn animate__faster"
       container
       spacing={0}
       direction="column"
