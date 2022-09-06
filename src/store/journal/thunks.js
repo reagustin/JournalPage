dec267c0a5d2086c4e1d2b5dc9bc4b412efadd64
Agustin <re_agustin@hotmail.com>
2022-09-05 23:34:24 -0300

    firestore

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,19 @@
+
+export const startNewNote = () => {
+
+    return async(dispatch) => {
+
+        //uid
+
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+
+        //!dispatch
+        // dispatch(newNote)
+        // dispatch(activarNote)
+
+    }
+}
