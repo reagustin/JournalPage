2512d9e40f462ec5ee59f728d6f8923d21f3f5c1
Agustin <re_agustin@hotmail.com>
2022-09-14 23:07:34 -0300

    error corregido

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import { ImageList, ImageListItem } from "@mui/material";
 
-export const ImageGallery = ({images}) => {
+export const ImageGallery = ({images = []}) => {
   return (
     <ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={164}>
       {images.map((image) => (
