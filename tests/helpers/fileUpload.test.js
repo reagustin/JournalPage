52d5545fd199fc7ca8ef60dfc8f59ba3f461226a
Agustin <re_agustin@hotmail.com>
2022-09-16 00:27:57 -0300

    testing

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -5,8 +5,7 @@ describe('Pruebas en el fileUpload', () => {
     
     test('debe de subir el archivo correctamente a clourdinary', async() => { 
 
-        //necesitamos una imagen para probar
-        
+        //necesitamos una imagen para probar        
         const imageUrl = 'https://www.pandaancha.mx/plds/articulos/4bf04095762666e2e6b288e55796834e585710389.jpg';
         const resp = await fetch(imageUrl);
         const blob = await resp.blob();
